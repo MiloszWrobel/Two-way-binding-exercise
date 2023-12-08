8c32c3453fb5d87a95f4e83d0b15301f2b6ebf31
MiloszWrobel <100210720+MiloszWrobel@users.noreply.github.com>
2023-12-08 15:13:05 +0000

    Added functionality to the name change

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ import Review from "./Review";
 // don't change the Component name "App"
 function App() {
   const [studentComment, setComment] = useState("");
-  function handleChange(event) {
+  const [studentName, setName] = useState("");
+  function handleCommentChange(event) {
     setComment(event.target.value);
+  }
+  function handleNameChange(event) {
+    setName(event.target.value);
   }
   return (
     <>
@@ -13,20 +17,31 @@ function App() {
         <h2>Please share some feedback</h2>
         <p>
           <label>Your Feedback</label>
-          <textarea onChange={handleChange} />
+          <textarea
+            type="text"
+            required
+            value={studentComment}
+            onChange={handleCommentChange}
+          />
         </p>
         <p>
           <label>Your Name</label>
-          <input type="text" />
+          <input
+            type="text"
+            required
+            value={studentName}
+            onChange={handleNameChange}
+          />
         </p>
       </section>
       <section id="draft">
         <h2>Your feedback</h2>
 
-        <Review feedback={studentComment} student="Student1" />
+        <Review feedback={studentComment} student={studentName} />
 
         <p>
           <button>Save</button>
+          {/*This is here for decorative purposes it has no functionality*/}
         </p>
       </section>
     </>
